d82bfb316ee9237017c4976855afb50d57e49e90
Carlos Danyel <154530577+carlosdanyelps@users.noreply.github.com>
2026-04-15 09:42:34 -0300

    Criei o componente seletor

--- interface_times/times-app/src/assets/Components/Header/Header.tsx
+++ interface_times/times-app/src/assets/Components/Header/Header.tsx
@@ -1,13 +1,52 @@
 // import React from 'react';
 import './Header.css'
 
+import React, { useState } from 'react';
+import SeletorTemp from '../Seletor/SeletorTemp';
+
+interface TimeData {
+  ano: number;
+}
 const Header = () => {
+  const [anoSelecionado, setAnoSelecionado] = useState<number>(2024);
+
+  const dados: TimeData[] = [
+    { ano: 2003 },
+    { ano: 2004 },
+    { ano: 2005 },
+    { ano: 2006 },
+    { ano: 2007 },
+    { ano: 2008 },
+    { ano: 2009 },
+    { ano: 2010 },
+    { ano: 2011 },
+    { ano: 2012 },
+    { ano: 2013 },
+    { ano: 2014 },
+    { ano: 2015 },
+    { ano: 2016 },
+    { ano: 2017 },
+    { ano: 2018 },
+    { ano: 2019 },
+    { ano: 2020 },
+    { ano: 2021 },
+    { ano: 2022 },
+    { ano: 2023 },
+    { ano: 2024 },
+  ];
+
+  const handleAnoChange = (ano: number) => {
+    setAnoSelecionado(ano);
+  }
+
+
   return (
-    <div className='header'>
+    <header className='header'>
       <div className="header-logo">
         <h1>LS</h1>
       </div>
-    </div>
+      <SeletorTemp dados={dados} onAnoChange={handleAnoChange} anoSelecionado={anoSelecionado} />
+    </header>
   )
 }
 
---
+++ interface_times/times-app/src/assets/Components/Seletor/SeletorTemp.tsx
@@ -0,0 +1,29 @@
+'use client'
+
+// import React from 'react'
+import './SeletorTemp.css'
+
+interface TimeData {
+    ano: number;
+}
+
+interface TimeSeletorProps {
+    dados: TimeData[];
+    onAnoChange: (ano: number) => void;
+    anoSelecionado: number;
+}
+
+const SeletorTemp = ({ dados, onAnoChange, anoSelecionado }: TimeSeletorProps) => {
+    return (
+        <select name="ano" id="temporada" value={anoSelecionado} onChange={(e) => onAnoChange(Number(e.target.value))}>
+          <option value="" disabled>Selecione uma temporada</option>
+          {dados.map((item) => (
+            <option key={item.ano} value={item.ano}>
+                {item.ano}
+            </option>
+          ))}
+        </select>
+    )
+}
+
+export default SeletorTemp
