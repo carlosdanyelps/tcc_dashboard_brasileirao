ae19a73acc2dd509fa2766887fedfaee62c65dcd
NicholasFerreira <nicholasferreira0401@gmail.com>
2026-04-19 23:56:29 -0300

    Update apis.py

--- interface_times/Brasileiraotcc/Rotas_api/apis.py
+++ interface_times/Brasileiraotcc/Rotas_api/apis.py
@@ -7,14 +7,17 @@ from Rotas_api.campeaosuporteapi import campeoes_geral
 from funcoes.campeoes import tabela_ano
 from Rotas_api.timemain import resumo_time
 from escudos.API_escudos import escudo
+from funcoes.estatistica import pontuacao_final_por_temporada, mid_derrota, mid_gol, mid_vitoria, mid_empate
 from flask_cors import CORS
 
 app = Flask(__name__)
 CORS(app)
 
+#################### Tabela de classificação por ano #####################
 @app.route('/tabela', methods=['GET'])
 def get_tabela():
     ano = request.args.get('ano')
+
     if not ano:
         return jsonify({
     'Campeoes todas temporadas': campeoes_geral.to_dict(orient='records')
@@ -42,6 +45,43 @@ def timemain():
 @app.route('/escudo/<int:id>')
 def get_escudo(id):
     return escudo(id)
+
+@app.route('/pontuacao_temporada')
+def get_pontuacao_por_temporada():
+    time = request.args.get('time')
+    ano = request.args.get('ano')
+
+    if not time:
+        return jsonify({'erro': 'Time não especificado'}), 400
+
+    pontos = pontuacao_final_por_temporada(time)
+    if ano:
+        try:
+            ano = int(ano)
+        except ValueError:
+            return jsonify({'erro': 'Ano inválido'}), 400
+
+        if ano not in pontos:
+            return jsonify({'erro': f'Pontuação não encontrada para o ano {ano}'}), 404
+        return jsonify({'time': time, 'ano': ano, 'pontos': pontos[ano]})
+
+    return jsonify({'time': time, 'pontos_por_temporada': pontos})
+
+@app.route('/estatisticas')
+def get_estatisticas():
+    time = request.args.get('time')
+
+    if not time:
+        return jsonify({'erro': 'Time não especificado'}), 400
+
+    estatisticas = {
+        'time': time,
+        'média_gols': mid_gol(time),
+        'média_vitórias': mid_vitoria(time),
+        'média_derrotas': mid_derrota(time),
+        'média_empates': mid_empate(time)
+    }
+    return jsonify(estatisticas)
 
 if __name__ == '__main__':
     app.run(debug=True)
